0997a0edf9f9b9e296f639991ae56f5a19db7326
Luis Alejandro Quiroga Gómez <lualquigo@gmail.com>
2022-09-29 18:13:00 -0500

    Add: Migrations for new User table

---
+++ webserver/seeders/20220929230559-demo-user.cjs
@@ -0,0 +1,30 @@
+const { faker } = require("@faker-js/faker");
+
+module.exports = {
+    async up(queryInterface, Sequelize) {
+        /**
+         * Add seed commands here.
+         *
+         * Example:
+         * await queryInterface.bulkInsert('People', [{
+         *   name: 'John Doe',
+         *   isBetaMember: false
+         * }], {});
+        */
+
+        await queryInterface.bulkInsert('Users', [{
+            first_name: faker.name.firstName(),
+            last_name: faker.name.lastName(),
+            email: "test@test.com",
+        }]);
+    },
+
+    async down(queryInterface, Sequelize) {
+        /**
+         * Add commands to revert seed here.
+         *
+         * Example:
+         * await queryInterface.bulkDelete('People', null, {});
+         */
+    }
+};
