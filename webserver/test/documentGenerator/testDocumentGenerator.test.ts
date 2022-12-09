c78347ad15fefa9431ca8f4e6767acf12ac279bd
Luis Alejandro Quiroga Gómez <lualquigo@gmail.com>
2022-12-09 09:14:06 -0500

    Package and lock updates

--- webserver/test/documentGenerator/testDocumentGenerator.test.ts
+++ webserver/test/documentGenerator/testDocumentGenerator.test.ts
@@ -6,39 +6,38 @@ import { faker } from "@faker-js/faker";
 describe("testing", () => {
     test("testing something", () => {
         generateInvoice(
-      {
-        client: {
-          id: 1,
-          firstName: faker.name.firstName(),
-          lastName: faker.name.lastName(),
-          address: faker.address.streetAddress(),
-          document: faker.random.numeric(9),
-          email: faker.internet.email(),
-          phone: faker.phone.number(),
-        },
-        generationDate: new Date(),
-        services: [
-          {
-            id: 1,
-            description: faker.lorem.paragraph(),
-            name: faker.company.name(),
-          }
-        ],
-        invoice: {
-          generationDate: new Date(),
-          adjustment: 10,
-          contractId: 1,
-          id: 1,
-          paymentDate: new Date(),
-          periodEnd: new Date(),
-          periodStart: new Date(),
-          status: 0,
-          suspensionDate: new Date(),
-          value: 1000,
-        },
-        qrCode: faker.random.numeric(9),
-        
-      },
+            {
+                client: {
+                    id: 1,
+                    firstName: faker.name.firstName(),
+                    lastName: faker.name.lastName(),
+                    address: faker.address.streetAddress(),
+                    document: faker.random.numeric(9),
+                    email: faker.internet.email(),
+                    phone: faker.phone.number(),
+                },
+                generationDate: new Date(),
+                services: [
+                    {
+                        id: 1,
+                        description: faker.lorem.paragraph(),
+                        name: faker.company.name(),
+                    },
+                ],
+                invoice: {
+                    generationDate: new Date(),
+                    adjustment: 10,
+                    contractId: 1,
+                    id: 1,
+                    paymentDate: new Date(),
+                    periodEnd: new Date(),
+                    periodStart: new Date(),
+                    status: 0,
+                    suspensionDate: new Date(),
+                    value: 1000,
+                },
+                qrCode: faker.random.numeric(9),
+            },
             (document) => {
                 document.pipe(fs.createWriteStream("temp/invoice_test.pdf"));
             }
